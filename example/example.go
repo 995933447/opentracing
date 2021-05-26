4a974d85bc89a4d605395ed60dc1ab35319aa6d2
root <root@localhost.localdomain>
2021-05-26 05:31:26 -0700

    opentracing

--- example/example.go
+++ example/example.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	opentracingUtil "github.com/995933447/opentracing"
+	"github.com/995933447/opentracing/tracer_config"
 	zipkinTracerConfig "github.com/995933447/opentracing/tracer_config/zipkin"
 	"github.com/opentracing/opentracing-go"
 	"os"
@@ -9,17 +11,29 @@ import (
 )
 
 func main()  {
-	hostName, _ := os.Hostname()
-	err := opentracingUtil.BuildDefaultGlobalTracer(
-		hostName,
-		"http://localhost:9411/api/v2/spans",
-		"hi-drone-app-gateway",
-		zipkinTracerConfig.AlwaysSampleType,
-	)
+	hostName, err := os.Hostname()
 	if err != nil {
 		panic(err)
 	}
+
+	var tracerConfig tracer_config.TracerConfig
+	theZipkinTracerConfig := zipkinTracerConfig.NewTracerConfig(
+		"http://localhost:9411/api/v2/spans",
+		"test-opentracing",
+		hostName,
+		zipkinTracerConfig.AlwaysSampleType,
+		)
+	tracerConfig = *theZipkinTracerConfig
+
+	err = opentracingUtil.BuildGlobalTracer(opentracingUtil.ZipKinTracerDriver, &tracerConfig)
+	if err != nil {
+		panic(err)
+	}
+
 	span := opentracing.StartSpan("test-span")
-	defer span.Finish()
 	time.Sleep(time.Second)
+	fmt.Println("ok.")
+	span.Finish()
+	// 底层reporter不会立即发送数据给broker, 给zipkin等待足够的时间发送span
+	time.Sleep(time.Second * 5)
 }
